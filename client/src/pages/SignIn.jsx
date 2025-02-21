60f81f92978036c18d5e7a30dfaa6655b2379309
Om15102003 <omg5628@gmail.com>
2025-02-21 23:42:14 +0530

    added redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
 import { Link,useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart,signInSuccess,signInFailure } from '../redux/user/userSlice';
+
 const SignIn = () => {
   const [formData,setFormData]=useState({});
-  const [error, setError]=useState(null);
-  const [loading,setLoading]=useState(false);
+  const {error,loading}=useSelector((state)=>state.user); 
   const navigate=useNavigate();
+  const dispatch=useDispatch();
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -12,10 +15,10 @@ const SignIn = () => {
     });
   };
   const handleSubmit=async(e)=>{
+    e.preventDefault();
     try{
-      e.preventDefault();
-    setLoading(true);
-    const res=await fetch('/api/auth/signin',{
+      dispatch(signInStart());
+      const res=await fetch('/api/auth/signin',{
       method:'POST',
       headers:{
         'Content-Type':'application/json',
@@ -24,16 +27,13 @@ const SignIn = () => {
     });
     const data=await res.json();
     if(data.success===false){
-      setError(data.message);
-      setLoading(false);
+      dispatch(signInFailure(data.message));
       return; 
     }
-    setLoading(false);
-    setError(null);
+    dispatch(signInSuccess(data));
     navigate('/');
     }catch(error){
-      setLoading(false);
-      setError(error.message);
+      dispatch(signInFailure(error.message));
     }
   }
   return (
@@ -45,7 +45,7 @@ const SignIn = () => {
         <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'> {loading ? 'Loading...' : 'Sign In'} </button>
       </form>
       <div className='flex gap-2 mt-5'>
-        <p>Dont an account</p>
+        <p>Dont an account?</p>
         <Link to={'/sign-up'}>
         <span className='text-blue-700'>Sign up</span>
         </Link>
