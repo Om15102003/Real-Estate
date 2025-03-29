0c6c5e281e608ce018f14f5bebcf268956610278
Om15102003 <omg5628@gmail.com>
2025-03-30 02:11:14 +0530

    updating the search ui

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import {useNavigate} from 'react-router-dom';
+import ListingItem from '../components/ListingItem.jsx';
 
 const Search = () => {
     const [sidebardata,setSidebardata]=useState({
@@ -14,6 +15,9 @@ const Search = () => {
     const [loading,setLoading]=useState(false);
     const [listings,setListings]=useState([]);
     const navigate=useNavigate();
+    
+    console.log(listings);
+    
     useEffect(()=>{
         const urlParams=new URLSearchParams(location.search);
         const searchTermFromUrl=urlParams.get('searchTerm');
@@ -124,8 +128,24 @@ const Search = () => {
             </form>
 
         </div>
-        <div className=''>
+        <div className='flex-1'>
             <h1 className='text-3xl font-semibold border-b p-3 text-slate-700 mt-5'>Listing results:</h1>
+            <div className='p-7 flex flex-wrap gap-4'>
+                {!loading && listings.length===0 && (
+                    <p className='text-xl text-slate-700'>No listings found!</p>
+                )}
+                {
+                    loading && (
+                        <p className='text-xl text-slate-700 text-center w-full'>Loading...</p>
+                    )
+                }
+
+                {!loading &&
+                    listings.length>0 &&
+                    listings.map((listing) => (
+                    <ListingItem key={listing._id} listing={listing} />
+                ))}
+            </div>
         </div>
     </div>
   )
