abb4663982790c2b71c4a3b4d5eeb16dfef08013
Om15102003 <omg5628@gmail.com>
2025-03-30 23:22:59 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+        About OmEstate
+      </h1>
+      <p className='mb-4 text-slate-700'>
+      At OmEstate, we believe finding or listing a property should be simple, transparent, and efficient. Our platform is designed to bridge the gap between property owners and potential buyers or renters, offering a seamless experience for all users. Whether you are looking to sell, rent, or discover your next home, OmEstate provides the tools and resources to make the process hassle-free. With intuitive listing creation, detailed search filters, and direct communication features, we empower users to take control of their real estate journey. Our goal is to create a trusted community where every interaction is straightforward, secure, and focused on delivering real results.
+      </p>
+      <p className='mb-4 text-slate-700'>OmEstate stands out by prioritizing user needs at every step. Sellers and landlords can effortlessly post listings with key details like pricing, location, amenities (including parking availability and furnished status), and high-quality images. For seekers, our search functionality allows you to filter properties based on your unique preferences, ensuring you find options that truly match your lifestyle. The platform's built-in contact system enables logged-in users to securely connect with landlords or agents via email, fostering meaningful conversations without compromising privacy. We’re committed to transparency—every listing is verified to maintain quality, and our user-friendly interface ensures everyone, from first-time renters to seasoned investors, can navigate the platform with ease.</p>
+      <p className='mb-4 text-slate-700'>
+      Behind OmEstate is a passion for innovation and community. We understand that real estate is more than just transactions—its about finding spaces where memories are made and futures are built. Whether you’re joining as a homeowner, tenant, or curious explorer, we invite you to be part of a platform that values trust, accessibility, and connection. Welcome to OmEstate—your journey to the perfect property starts here.
+      </p>
+    </div>
   )
 }
 
