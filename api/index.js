165194d30fd7428c8edcd730afa1eb02b48520c1
Om15102003 <omg5628@gmail.com>
2025-03-30 23:48:00 +0530

    fixed index.js for render

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
 }).catch((err)=>{
     console.log(err)
 })
-const __dirname=dir.resolve();
+const __dirname=path.resolve();
 
 const app = express()
 app.use(express.json());
