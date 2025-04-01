8191a07a32bca0c69a6893e0a53318e0656655a2
Om15102003 <omg5628@gmail.com>
2025-04-01 17:08:13 +0530

    fixing the typo

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -17,10 +17,10 @@ const ListingItem = ({listing}) => {
             <p className='text-slate-500 font-semibold mt-2'>₹{listing.offer?listing.discountPrice.toLocaleString('en-US'):listing.regularPrice.toLocaleString('en-US')}{listing.type==='rent'?' / month':""}</p>
             <div className='text-slate-700 flex gap-4' >
                 <div className='font-bold text-xs'>
-                    {listing.bedrooms>1?`${listing.bedrooms} beds`:`${listing.bedrooms} bed}`}
+                    {listing.bedrooms>1?`${listing.bedrooms} beds`:`${listing.bedrooms} bed`}
                 </div>
                 <div className='font-bold text-xs'>
-                    {listing.bathrooms>1?`${listing.bathrooms} baths`:`${listing.bathrooms} bath}`}
+                    {listing.bathrooms>1?`${listing.bathrooms} baths`:`${listing.bathrooms} bath`}
                 </div>
             </div>
         </div>
