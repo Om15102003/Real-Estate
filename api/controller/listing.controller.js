67dd09ec37ad015e85db0f62c925ac3ccb5377d6
Om15102003 <omg5628@gmail.com>
2025-04-05 16:30:20 +0530

    updated the search functionality

--- api/controller/listing.controller.js
+++ api/controller/listing.controller.js
@@ -84,7 +84,7 @@ export const getListings=async (req,res,next)=>{
         const sort=req.query.sort||'createdAt';
         const order=req.query.order||'desc';
         const listings=await Listing.find({
-            name:{$regex:searchTerm,$options:'i'},
+            address:{$regex:searchTerm,$options:'i'},
             furnished,
             type,
             offer,
